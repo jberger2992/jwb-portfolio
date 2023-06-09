2f173e3e89e391234ff1faf08b394bf326bc3740
jberger2992 <jberger2992@gmail.com>
2023-06-08 21:39:19 -0400

    minor project change

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,8 +3,7 @@ import React from "react";
 function Project(props){
     return(<>
         <div>
-            <h3>{props.name}</h3>
-            <a href={props.deployed} target="_blank" rel="noreferrer">Link</a>
+            <a href={props.deployed} target="_blank" rel="noreferrer">{props.name}</a>
             <br />
             <a href={props.repo} target="_blank" rel="noreferrer">Repo</a>
         </div>
